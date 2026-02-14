f564d0f18d36b776526b6f348c8fe375a7869020
AlejandroMevi <mevi.and.my.monkey@gmail.com>
2026-02-13 22:07:12 -0600

    chore(release): Actualizar versión de la aplicación a 1.0.1
    Este commit actualiza el código y el nombre de la versión de la aplicación.
    
    **Cambios específicos:**
    
    -   **`app/build.gradle.kts`**:
        -   Se ha incrementado `versionCode` de 1 a 2.
        -   Se ha actualizado `versionName` de "1.0.0" a "1.0.1".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
-    id("com.google.dagger.hilt.android") // Hilt plugin
+    id("com.google.dagger.hilt.android")
     kotlin("kapt")
     alias(libs.plugins.jetbrainsKotlinSerialization)
     alias(libs.plugins.kotlin.parcelize)
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mevi.lasheslam"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
